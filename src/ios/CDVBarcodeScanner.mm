78e733fba77e85a2d8509aba036f0d2bef268862
Ted Ramert <ted@BumpMe.co>
2021-04-22 21:10:17 -0500

    making square smaller again again again

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -953,9 +953,9 @@ parentViewController:(UIViewController*)parentViewController
 //--------------------------------------------------------------------------
 
 #define RETICLE_SIZE    100.0f
-#define RETICLE_WIDTH    6.0f
+#define RETICLE_WIDTH    2.0f
 #define RETICLE_OFFSET   60.0f
-#define RETICLE_ALPHA     0.8f
+#define RETICLE_ALPHA     1.0f
 #define RETICLE_CLEAR     0.0f
 
 //-------------------------------------------------------------------------
